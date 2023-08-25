8f52138187d379d02f0fa39d6e6af67c877f2062
Santellana179 <imjuan179@gmail.com>
2023-08-25 02:50:08 -0500

    Agregar archivos de proyecto.

---
+++ programas para calcular el factorial/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Ingresa un número: ");
+int num = int.Parse(Console.ReadLine());
+
+int factorial = CalcularFactorialIterativo(num);
+Console.WriteLine($"El factorial de {num} es {factorial}");
+        
+
+static int CalcularFactorialIterativo(int n)
+{
+    int resultado = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        resultado *= i;
+    }
+    return resultado;
+}
